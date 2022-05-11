e022699aafeac98dca3a325f897239ef36e70ca6
youth2ee <sprirealee@gmail.com>
2022-05-12 00:53:05 +0900

    Initial commit

---
+++ src/main/java/com/iteratrlearning/shu_book/chapter_02/MainApplication.java
@@ -0,0 +1,15 @@
+package com.iteratrlearning.shu_book.chapter_02;
+
+import java.io.IOException;
+
+public class MainApplication {
+
+	public static void main(String[] args) throws IOException {
+
+		 final BankStatementAnalyzer bankStatementAnalyzer = new BankStatementAnalyzer();
+		 final BankStatementParser bankStatementParser = new BankStatementCSVParser();
+		 bankStatementAnalyzer.analyze("bank-data-simple.csv", bankStatementParser);
+		
+	}
+
+}
